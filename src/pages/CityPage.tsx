def65d96bcb1f5a9c97570e5690f8cb60847f12d
Vishal Kumar <vishalgatishifting@gmail.com>
2026-05-22 16:34:43 +0530

    upgrade

--- src/pages/CityPage.tsx
+++ src/pages/CityPage.tsx
@@ -372,7 +372,7 @@ const CityPage = () => {
   const img = cityImage.img;
   const metaData = cityMeta.metaDetails;
   const isSpecial = SPECIAL_CITIES.includes(city);
-
+  console.log(city)
   return (
     <>
       <Helmet>
@@ -385,16 +385,17 @@ const CityPage = () => {
         <meta property="og:description" content={metaData?.ogDescription} />
         <meta property="og:url" content={`https://safeshiftingpackers.com/city/${slug}`} />
         <meta property="og:site_name" content="Safexpress" />
-        <meta property="og:image" content={`https://safeshiftingpackers.com/${img}`} />
+        <meta property="og:image" content={`https://safeshiftingpackers.com/cityPages/${img}.webp`} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={metaData?.title} />
         <meta name="twitter:description" content={metaData?.twitterDescription} />
         <meta name="twitter:image" content={img} />
         <meta http-equiv="Content-Language" content="en" />
-        <link rel="canonical" href={`https://safeshiftingpackers.com/city/${slug}`} />
+        <link rel="canonical" href={`https://safeshiftingpackers.com/cityPages/${img}.webp`} />
 
 
       </Helmet>
+      
       <CitySchema cityMeta={cityMeta} />
       <main className="cp-root">
         {isSpecial ? (
